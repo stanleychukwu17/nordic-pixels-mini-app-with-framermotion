98d41a62de416cf0852f489055b74d82e17d59ab
Chukwu Stanley <stanleychukwu17@yahoo.com>
2023-02-13 11:01:36 +0100

    cleaned the app up and ready to continue building, been a minute.. but am ready to take over the world now!

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,25 @@
 import './app.scss';
-import { motion } from 'framer-motion';
-import { gsap } from 'gsap';
+// import { motion } from 'framer-motion';
+// import { gsap } from 'gsap';
+
+const images = [
+]
+for (let index = 0; index < 20; index++) {
+    images.push({
+        img: require(`../../assets/img${index}.jpg`)
+    })
+}
+console.log(images)
 
 const App = () => {
-
     return (
         <div className="AppMain">
+            <div className="">
+                <span className="first">Nordic</span> <span className="second">Pixels</span>
+            </div>
+            <div className="">
 
+            </div>
         </div>
     )
 }
