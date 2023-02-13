436d0a6b55de6e9c85368375166daa6220942810
Chukwu Stanley <stanleychukwu17@yahoo.com>
2023-02-13 11:35:46 +0100

    done with loading the images, now time to style them so that we can get exactly what we want

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,24 +1,36 @@
+import { useState } from 'react';
 import './app.scss';
 // import { motion } from 'framer-motion';
 // import { gsap } from 'gsap';
 
-const images = [
-]
-for (let index = 0; index < 20; index++) {
+type imgType = {num:number, img:string}
+const images: imgType[] = []
+
+
+for (let index = 1; index < 13; index++) {
     images.push({
-        img: require(`../../assets/img${index}.jpg`)
+        num:index,
+        img: require(`../../assets/images/${index}.jpg`)
     })
 }
 console.log(images)
 
 const App = () => {
+    const [allImages, setAllImages] = useState<imgType[]>(images)
+
     return (
         <div className="AppMain">
             <div className="">
                 <span className="first">Nordic</span> <span className="second">Pixels</span>
             </div>
             <div className="">
-
+                {allImages.map((ech, index) => {
+                    return (
+                        <div className="" key={index}>
+                            <img src={ech.img} alt="" />
+                        </div>
+                    )
+                })}
             </div>
         </div>
     )
