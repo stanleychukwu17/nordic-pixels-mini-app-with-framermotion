a8a9b981f0e7925213de1bc638263d9a8a5eaf0e
Chukwu Stanley <stanleychukwu17@yahoo.com>
2023-02-15 21:07:18 +0100

    finished working on the mini-window to show the images

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import './app.scss';
 // import { motion } from 'framer-motion';
 // import { gsap } from 'gsap';
+import { FaChevronLeft } from "react-icons/fa";
+import { FaChevronRight } from "react-icons/fa";
 
 type imgType = {num:number, img:string}
 const images: imgType[] = []
@@ -48,8 +50,23 @@ const App = () => {
                     <div className="orderClose"><p className=""></p></div>
                     <div className="box1">
                         <div className="orderTitle">Choose your format</div>
-                        <div className="orderImgCvr"></div>
-                        <div className="orderBtn"></div>
+                        <div className="orderImgCvr">
+                            <div className="navBtn">
+                                <div className=""><FaChevronLeft /></div>
+                            </div>
+                            <div className="imgSlideCvr">
+                                <div className="imgInnerCvr"></div>
+                            </div>
+                            <div className="navBtn">
+                                <div className=""><FaChevronRight /></div>
+                            </div>
+                        </div>
+                        <div className="imgFormatB1">
+                            <div className="imgFormatCvr"></div>
+                        </div>
+                        <div className="orderBtn">
+                            <button className="">Continue</button>
+                        </div>
                     </div>
                     <div className="box2"></div>
                 </div>
