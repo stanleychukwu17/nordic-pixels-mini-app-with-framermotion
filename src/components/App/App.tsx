856447a28ee8f63d09b75874564ba9f66d22ae78
Chukwu Stanley <stanleychukwu17@yahoo.com>
2023-02-16 07:40:40 +0100

    moving the image boxes into a separate component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,8 @@
 import { useCallback, useState } from 'react';
 import './app.scss';
-// import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 import { gsap } from 'gsap';
+
 import { FaChevronLeft } from "react-icons/fa";
 import { FaChevronRight } from "react-icons/fa";
 import { FaStar } from "react-icons/fa";
@@ -22,19 +23,42 @@ for (let index = 1; index < 13; index++) {
     images.push({
         id: index,
         img: require(`../../assets/images/${index}.jpg`),
-        // img: require(`../../assets/images/1.jpg`),
         rating: Math.round(gsap.utils.random(1, 20)),
         views: Math.round(gsap.utils.random(10, 5000)),
     })
 }
 
+
+
+
+
+
+
+
+type imgProps = {
+    obj: singleImgType,
+    index: number
+}
+const ImgComp = ({obj, index}: imgProps) => {
+    return (
+        <motion.div layout className="ImgBoth" key={`${obj.key}-${obj.title}`} variants={cardVariant} initial="initial" animate="animate" custom={index}>
+            <motion.div className='ImgCvr' >
+                <motion.img src={obj.img} alt="" variants={imgMainVar} />
+            </motion.div>
+            <motion.div className="ImgTitle" variants={imgTitleVar}>{obj.title} - {obj.genre.join(' ')}</motion.div>
+        </motion.div>
+    )
+}
+
 const App = () => {
     const [allImages, setAllImages] = useState<imgType[]>(images)
+    const [tabActive, setTabActive] = useState<'none'|'views'|'rating'>('none')
 
     // sort the images in accordance to the selected tab
-    const show_the_images_for_this_tab = useCallback((wchTab:string) => {
+    const show_the_images_for_this_tab = useCallback((wchTab:'views'|'rating') => {
         const newSort = _.sortBy(allImages, [wchTab]).reverse()
         setAllImages(newSort)
+        setTabActive(wchTab)
     }, [])
 
     return (
@@ -45,21 +69,30 @@ const App = () => {
                 </div>
                 <div className="navBars">
                     <div>
-                        <div className='navClick' onClick={()=> show_the_images_for_this_tab('rating')}><span><FaStar /></span> Most Rated</div>
+                        {
+                            tabActive === 'rating' ? (<div className='navClick active' onClick={()=> show_the_images_for_this_tab('rating')}><span><FaStar /></span> Most Rated</div>)
+                                                    : (<div className='navClick' onClick={()=> show_the_images_for_this_tab('rating')}><span><FaStar /></span> Most Rated</div>)
+                        }
                     </div>
                     <div>
-                        <div className='navClick' onClick={()=> show_the_images_for_this_tab('views')}><span><FaRegEye /></span> Most viewed</div>
+                        {
+                            tabActive === 'views' ? (<div className='navClick active' onClick={()=> show_the_images_for_this_tab('views')}><span><FaRegEye /></span> Most viewed</div>)
+                                                    : (<div className='navClick' onClick={()=> show_the_images_for_this_tab('views')}><span><FaRegEye /></span> Most viewed</div>)
+                        }
                     </div>
                 </div>
             </div>
             <div className="AppImages">
-                {allImages.map((ech, index) => {
-                    return (
-                        <div className="" key={index} data-id={ech.id} data-rating={ech.rating} data-views={ech.views}>
-                            <img src={ech.img} alt="" />
-                        </div>
-                    )
-                })}
+                    {allImages.map((ech, index) => {
+                        return (
+                            <motion.div
+                                layout
+                                className="" key={index} data-id={ech.id} data-rating={ech.rating} data-views={ech.views}
+                            >
+                                <img src={ech.img} alt="" />
+                            </motion.div>
+                        )
+                    })}
             </div>
             <div className="orderWindow"> 
                 <div className="order_mini_window">
