bfa07d902a1190f0b820eea002d23ab02daa3c87
Chukwu Stanley <stanleychukwu17@yahoo.com>
2023-02-18 09:53:28 +0100

    finished the styling of box2 or confirming of purchase worked on clicking of the button so that we can switch from box1 to box2

--- src/components/modal/MiniWindow.tsx
+++ src/components/modal/MiniWindow.tsx
@@ -1,8 +1,11 @@
+import {useState } from 'react';
 import './MiniStyle.scss'
 
 // icons to use from react-icons
 import { FaChevronLeft } from "react-icons/fa";
 import { FaChevronRight } from "react-icons/fa";
+import { FaAngleDoubleRight } from "react-icons/fa";
+
 
 
 type miniProps = {
@@ -11,36 +14,55 @@ type miniProps = {
 }
 // https://learn.headliner.app/hc/en-us/articles/360004101114-What-are-the-sizes-of-the-landscape-portrait-square-templates- - got references for different sizes from here
 export default function MiniWindow({imgUrl}:miniProps) {
+    const [showBox2, setShowBox2] = useState<boolean>(false)
 
     return (
         <div className="orderWindow">
             <div className="order_mini_window">
                 <div className="orderClose"><p className=""></p></div>
-                <div className="box1">
-                    <div className="orderTitle">Choose your format</div>
-                    <div className="orderImgCvr">
-                        <div className="navBtn">
-                            <div className=""><FaChevronLeft /></div>
-                        </div>
-                        <div className="imgSlideCvr">
-                            <div className="imgInnerCvr">
-                                <div className="img1"><img src={imgUrl} alt="" /></div>
-                                <div className="img1"><img src={imgUrl} alt="" /></div>
-                                <div className="img1"><img src={imgUrl} alt="" /></div>
+                {!showBox2 && (
+                    <div className="box1">
+                        <div className="orderTitle">Choose your format</div>
+                        <div className="orderImgCvr">
+                            <div className="navBtn">
+                                <div className=""><FaChevronLeft /></div>
+                            </div>
+                            <div className="imgSlideCvr">
+                                <div className="imgInnerCvr">
+                                    <div className="img1"><img src={imgUrl} alt="" /></div>
+                                    <div className="img1"><img src={imgUrl} alt="" /></div>
+                                    <div className="img1"><img src={imgUrl} alt="" /></div>
+                                </div>
+                            </div>
+                            <div className="navBtn">
+                                <div className=""><FaChevronRight /></div>
                             </div>
                         </div>
-                        <div className="navBtn">
-                            <div className=""><FaChevronRight /></div>
+                        <div className="imgFormatB1">
+                            <div className="imgFormatCvr"></div>
+                        </div>
+                        <div className="orderBtn">
+                            <button onClick={() => setShowBox2(true)}>Continue</button>
                         </div>
                     </div>
-                    <div className="imgFormatB1">
-                        <div className="imgFormatCvr"></div>
+                )}
+                {showBox2 && (
+                    <div className="box2">
+                        <div className="orderTitle">Review your order</div>
+                        <div className="bx2Mid">
+                            <div className=""><p>SKU:</p><p>Printed photo</p></div>
+                            <div className=""><p>Delivery:</p><p>Expedited</p></div>
+                            <div className=""><p>Price:</p><p>$59.99</p></div>
+                        </div>
+
+                        <div className="bx2Btn">
+                            <button>
+                                <p><FaAngleDoubleRight /></p>
+                                Confirm your order
+                            </button>
+                        </div>
                     </div>
-                    <div className="orderBtn">
-                        <button className="">Continue</button>
-                    </div>
-                </div>
-                <div className="box2"></div>
+                )}
             </div>
         </div>
     )
